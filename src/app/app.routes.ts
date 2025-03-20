5ea79e2833f0b15d8c7184441fd55ef2ab27d39a
Ajaykumarjabu <jabu.ajay@gmail.com>
2025-03-20 15:19:16 +0530

    Added routing configuration for welcome and dashboard pages with nested routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import { Routes } from '@angular/router';
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
+import { DashboardpageComponent } from './dashboardpage/dashboardpage.component';
+import { LoginComponent } from './welcomepage/login/login.component';
+import { RegisterComponent } from './welcomepage/register/register.component';
+import { HomeComponent } from './dashboardpage/home/home.component';
+import { ProfileComponent } from './dashboardpage/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    {
+        path:'',
+        redirectTo:'welcomepage',
+        pathMatch:'full'
+    },
+
+    {
+        path:'welcomepage',
+        component:WelcomepageComponent,
+        children:
+        [
+            {
+                path:'',
+                redirectTo:'login',
+                pathMatch:'full'
+            },
+
+            {   path:'login',
+                component:LoginComponent 
+            },
+
+            {
+                path:'register',
+                component:RegisterComponent
+            }
+        ]
+    },
+
+    {
+        path:'dashboard',
+        component:DashboardpageComponent,
+        children:
+        [
+            {
+                path:'',
+                redirectTo:'home',
+                pathMatch:'full'
+            },
+
+            {   path:'home',
+                component:HomeComponent 
+            },
+
+            {
+                path:'profile',
+                component:ProfileComponent
+            }
+        ]
+    }
+];
