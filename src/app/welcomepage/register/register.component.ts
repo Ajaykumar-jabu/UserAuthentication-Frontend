c65cefd232043bb47d29f7c1a3c34755df1f151c
Ajaykumarjabu <jabu.ajay@gmail.com>
2025-03-21 11:49:34 +0530

    Implemented registration component with reactive forms, validation, and HTTP service integration

--- src/app/welcomepage/register/register.component.ts
+++ src/app/welcomepage/register/register.component.ts
@@ -1,11 +1,68 @@
-import { Component } from '@angular/core';
+
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { HttpService } from '../../http.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent 
+{
+
+  backendMessage:any="";
+  serviceToComponet:any=inject(HttpService);
+  postToUrl:string="http://localhost:8080/register";
+  userForms:FormGroup;
+
+    constructor()
+    {
+      this.userForms=new FormGroup({
+        email:new FormControl('', [Validators.required,Validators.email]),
+        fullname:new FormControl('', [Validators.required, Validators.minLength(4)]),
+        username:new FormControl('', [Validators.required,Validators.minLength(3)]),
+        password:new FormControl('',[Validators.required, Validators.minLength(8),Validators.pattern(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)]),
+        location:new FormControl('add location'),
+        bio:new FormControl('add bio')
+      
+      })
+    }
+
+    onSubmit()
+    {
+      this.serviceToComponet.postUser(this.postToUrl,this.userForms.value).subscribe
+      ( 
+            (succesful: any) =>
+            {
+                console.log('User Created:', succesful);
+                this.backendMessage=succesful;
+                setTimeout(() => {
+                this.backendMessage="";
+                }, 5000);
+                this.userForms.reset();
+            },
+            (error: any) =>
+            {
+                console.error('Error:', error);
+                if(error.error instanceof ProgressEvent)
+                {
+                    this.backendMessage="Server is down, Please try again later!";
+                    setTimeout(() => {
+                        this.backendMessage="";
+                    }, 5000);
+                }
+                else
+                {
+                    this.backendMessage=error.error|| "Something went wrong!"; 
+                    setTimeout(() => {
+                        this.backendMessage="";
+                    }, 5000);
+                }
+            }
+      );
+    }
 
 }
