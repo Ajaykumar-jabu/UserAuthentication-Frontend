cfd8b44dd555dbcc3689d967e8e1b5b3fabb0952
Ajaykumarjabu <jabu.ajay@gmail.com>
2025-03-24 19:50:16 +0530

    Implemented dashboard page layout with responsive design, navigation, and logout functionality

--- src/app/dashboardpage/dashboardpage.component.ts
+++ src/app/dashboardpage/dashboardpage.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet,Router } from '@angular/router';
 
 @Component({
-  selector: 'app-dashboardpage',
-  imports: [],
+  selector: 'app-dashboard',
+  imports: [RouterLink, RouterOutlet,],
   templateUrl: './dashboardpage.component.html',
   styleUrl: './dashboardpage.component.css'
 })
-export class DashboardpageComponent {
+export class DashboardpageComponent 
+{
+    isNavOpen = false;
+    navSessionAction:string = sessionStorage.getItem("username") ? 'Logout' : 'Login';
 
+    constructor(private router: Router) { }
+
+    logout() 
+    {
+        console.log("Logout");
+        sessionStorage.removeItem("username");
+        setTimeout(() => 
+        {
+            this.router.navigate(['welcomepage/login']);
+        }, 800);
+    }
+
+    toggleNav() 
+    {
+        this.isNavOpen = !this.isNavOpen;
+    }
 }
