debbb7b01c92aa2046f223f0e7f57b738924a417
Ajaykumarjabu <jabu.ajay@gmail.com>
2025-03-24 20:13:18 +0530

    Implemented home component with responsive design, user greeting, and dynamic content loading from HttpService

--- src/app/dashboardpage/home/home.component.ts
+++ src/app/dashboardpage/home/home.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpService } from '../../http.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent 
+{
+    serviceToComponet: any = inject(HttpService);
+    username: any = sessionStorage.getItem("username");
+    getFromUrl: string = `http://localhost:8080/dashboard/${this.username}`;
+    userData: any;
+
+    constructor() 
+    {
+        this.serviceToComponet.getUser(this.getFromUrl).subscribe(
+
+          (success: any) => {
+            this.userData = success;
+            console.log('User Data:', success);
+          },
+          (error: any) => {
+            console.error('Error:', error);
+          }
+        )
+    }
+
+    greetUser():string
+    {
+        const dateRefvar= new Date();
+        let hours = dateRefvar.getHours();
+
+        if(hours < 12)      { return 'Good Morning';}
+        else if(hours < 18) { return 'Good Afternoon';}
+        else                { return 'Good Evening';}
+    }
 
 }
